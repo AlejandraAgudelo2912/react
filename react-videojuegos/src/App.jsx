27594481ec34a5628e66bdf6dfda258ef091664d
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-06-04 22:58:10 +0200

    Creacion y utilizacion del primer componente

--- react-videojuegos/src/App.jsx
+++ react-videojuegos/src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
+import Videojuego from "./components/Videojuego";
 
 function App (){
+      const videojuegos = [
+        {
+            nombre: "Minecraft",
+            horas: 500
+        },
+        {
+            nombre: "Terraria",
+            horas: 200
+        },
+        {
+            nombre: "Stardew Valley",
+            horas: 150
+        }
+    ];
   return (
     <div>
       <h1>Biblioteca De Videojuegos</h1>
-      <p>Mi primer proyecto en React</p>
+      {videojuegos.map(videojuego => (
+        <Videojuego 
+          nombre={videojuego.nombre}
+          horas={videojuego.horas}
+        />      
+      ))}
     </div>
     
   )
---
+++ react-videojuegos/src/components/Videojuego.jsx
@@ -0,0 +1,9 @@
+function Videojuego({nombre, horas}){
+    return (
+        <div>
+            <h2>{nombre}</h2>
+            <p>Horas jugadas: {horas}</p>
+        </div>
+    )
+}
+export default Videojuego;
